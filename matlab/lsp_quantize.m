1f7383c0053d0737d856988384c55d96dcec3717
astkalx <astkalx@yandex.ru>
2025-07-13 06:24:34 +0300

    Updated lsp_quantize (performance improved)

--- matlab/lsp_quantize.m
+++ matlab/lsp_quantize.m
@@ -14,35 +14,31 @@ function [quant_lsp, bits] = lsp_quantize(lsp, state, phon_class)
     CB3 = state.lsp_cb.stage3;
     
     % Этап 1: Первые 4 LSP (взвешенное квантование)
-    [idx1, quant1] = weighted_vq(lsp(1:4)', CB1, weights(1:4));
+    [idx1, quant1] = fast_weighted_vq(lsp(1:4)', CB1, weights(1:4));
     bits1 = de2bi(idx1-1, 18, 'left-msb');
     
     % Этап 2: Следующие 4 LSP
-    [idx2, quant2] = weighted_vq(lsp(5:8)', CB2, weights(5:8));
+    [idx2, quant2] = fast_weighted_vq(lsp(5:8)', CB2, weights(5:8));
     bits2 = de2bi(idx2-1, 18, 'left-msb');
     
     % Этап 3: Последние 8 LSP
-    [idx3, quant3] = weighted_vq(lsp(9:16)', CB3, weights(9:16));
+    [idx3, quant3] = fast_weighted_vq(lsp(9:16)', CB3, weights(9:16));
     bits3 = de2bi(idx3-1, 18, 'left-msb');
     
     quant_lsp = [quant1; quant2; quant3];
     bits = [bits1, bits2, bits3];
 end
 
-function [idx, quant] = weighted_vq(vec, codebook, weights)
-    % Взвешенное векторное квантование
-    min_dist = inf;
-    idx = 1;
+function [idx, quant] = fast_weighted_vq(vec, codebook, weights)
+    % Предварительное вычисление квадратов весов
+    weights = weights(:)';
+    weighted_weights = weights .* weights;
     
-    for i = 1:size(codebook, 1)
-        diff = vec - codebook(i,:);
-        dist = sum(weights .* (diff.^2));
-        
-        if dist < min_dist
-            min_dist = dist;
-            idx = i;
-        end
-    end
+    % Оптимизированный векторный расчет
+    dist = sum(weighted_weights .* (codebook.^2), 2) - ...
+           2 * (codebook * (vec .* weights)') + ...
+           sum(weights .* (vec.^2));
     
+    [~, idx] = min(dist);
     quant = codebook(idx, :)';
 end
